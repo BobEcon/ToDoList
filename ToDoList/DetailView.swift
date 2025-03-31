81535b033597390fce064652cf8e38364383483f
Robert Beachill <bbeachill@icloud.com>
2025-04-01 00:15:27 +0100

    Added a new View on a separate screen And a navigation link to and from the new sceen

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,36 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Robert Beachill on 31/03/2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.dismiss) private var dismiss //use this line
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("You Are a Swifty Legend!")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button("Get Back!") {
+                dismiss() //and this to dismiss a screen
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+        .navigationBarBackButtonHidden()
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ ToDoList/ToDoListView.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoListView.swift
+//  ToDoList
+//
+//  Created by Robert Beachill on 31/03/2025.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                NavigationLink {
+                    DetailView()
+                } label: {
+                    Image(systemName: "eye")
+                    Text("Show the New View!")
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
